b06454718f8aa72986602bff5b2892770f9e85cd
web25Lucius <web25Lucius@gmail.com>
2021-04-12 00:39:04 -0600

    last get request

--- api/server.js
+++ api/server.js
@@ -1,2 +1,17 @@
+const express = require("express")
+const posts = require('./posts/posts-model')
+const postsRouter = require('./posts/posts-router')
+const server = express()
+
+// = express.server()
+
+server.use(express.json()) //< --must include to parse from JSON
+server.use(postsRouter)
+
+server.get("/", (req, res) => {
+    res.json({ message: "Yo Api2 project!", })
+})
+
+module.exports = server
 // implement your server here
 // require your posts router and connect it here
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const server = require('./api/server');
+
+const port = 1234;
+
 // require your server and launch it here
+server.listen (port, () => {
+	console.log("running at http://localhost:1234")
+})
